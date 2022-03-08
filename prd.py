3ee36e884c4a3ad27fa420d314f3f537a54528ce
grok13 <grok13>
2022-03-09 00:44:27 +0100

    Add partial saves

--- prd.py
+++ prd.py
@@ -2600,7 +2600,7 @@ else:
 
 # @markdown ####**Saving:**
 
-intermediate_saves = 0  # @param{type: 'raw'}
+intermediate_saves = 5  # @param{type: 'raw'}
 intermediates_in_subfolder = True  # @param{type: 'boolean'}
 # @markdown Intermediate steps will save a copy at your specified intervals. You can either format it as a single integer or a list of specific steps
 
